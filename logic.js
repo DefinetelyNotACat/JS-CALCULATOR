691bcd5707bb5845bf4b22e801a9f87b3c3e4cdc
DefinetelyNotACat <75591664+DefinetelyNotACat@users.noreply.github.com>
2024-01-04 12:23:34 +0100

    NUMBERS WORK NOW

---
+++ logic.js
@@ -0,0 +1,11 @@
+// let a = 5
+// let b = 10
+// let sum = String(a) + String(b)
+// let intsum = parseInt(sum)
+// console.log(intsum)
+
+function getnumber(numid) {
+     let nums = numid
+     console.log(nums)
+   
+}
